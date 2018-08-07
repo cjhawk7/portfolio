006f8ce6c9767f708e6accfac5f83955fe20bc86
Christopher Johnson <cjhawk7@hotmail.com>
2018-08-07 13:05:34 -0700

    actually fixed footer correctly

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ $(".mainbutton").on("click", function() {
   $(".projectpage").removeClass("hidden");
   $(".mainpage").removeClass("hidden");
 });
+
+function footerAlign() {
+  $(".footer").css("display", "block");
+  $(".footer").css("height", "auto");
+  var footerHeight = $(".footer").outerHeight();
+  $("body").css("padding-bottom", footerHeight);
+  $(".footer").css("height", footerHeight);
+}
+
+$(document).ready(function() {
+  footerAlign();
+});
+
+$(window).resize(function() {
+  footerAlign();
+});
